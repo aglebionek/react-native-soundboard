90366685ca793cefdb483e371f5aba4829bd331f
aglebionek <aglebionek@gmail.com>
2025-04-09 19:31:51 +0200

    rename audiofiles to use '_' instead of '-' per gradlew build requirements, add android build command to package.json

--- constants/defaultAudioData.ts
+++ constants/defaultAudioData.ts
@@ -4,72 +4,72 @@ const defaultData: AudioData[] = [
     {
         title: "Magic calls",
         isFavorite: false,
-        uri: "chattur/audio-0.mp3"
+        uri: "chattur/audio_0.mp3"
     },
     {
         title: "Even now",
         isFavorite: false,
-        uri: "chattur/audio-1.mp3"
+        uri: "chattur/audio_1.mp3"
     },
     {
         title: "Veigar laugh 1",
         isFavorite: false,
-        uri: "chattur/audio-2.mp3"
+        uri: "chattur/audio_2.mp3"
     },
     {
         title: "Veigar laugh 2",
         isFavorite: false,
-        uri: "chattur/audio-3.mp3"
+        uri: "chattur/audio_3.mp3"
     },
     {
         title: "Long whimper",
         isFavorite: false,
-        uri: "chattur/audio-4.mp3"
+        uri: "chattur/audio_4.mp3"
     },
     {
         title: "To jest komar...",
         isFavorite: false,
-        uri: "chattur/audio-5.mp3"
+        uri: "chattur/audio_5.mp3"
     },
     {
         title: "Jeb!",
         isFavorite: false,
-        uri: "chattur/audio-6.mp3"
+        uri: "chattur/audio_6.mp3"
     },
     {
         title: "Łe łe łe",
         isFavorite: false,
-        uri: "chattur/audio-7.mp3"
+        uri: "chattur/audio_7.mp3"
     },
     {
         title: "Woof, woof!",
         isFavorite: false,
-        uri: "chattur/audio-8.mp3"
+        uri: "chattur/audio_8.mp3"
     },
     {
         title: "Hic!",
         isFavorite: false,
-        uri: "chattur/audio-9.mp3"
+        uri: "chattur/audio_9.mp3"
     },
     {
         title: "Fiddlesticks",
         isFavorite: false,
-        uri: "chattur/audio-10.mp3"
+        uri: "chattur/audio_10.mp3"
     },
     {
         title: "Meow",
         isFavorite: false,
-        uri: "chattur/audio-11.mp3"
+        uri: "chattur/audio_11.mp3"
     },
     {
         title: "Weee",
         isFavorite: false,
-        uri: "chattur/audio-12.mp3"
+        uri: "chattur/audio_12.mp3"
     },
     {
         title: "Nyyuuuum",
         isFavorite: false,
-        uri: "chattur/audio-13.mp3"
+        uri: "chattur/audio_13.mp3"
     }
 ]
 
--- constants/soundMap.ts
+++ constants/soundMap.ts
@@ -1,20 +1,20 @@
 import { AVPlaybackSource } from "expo-av";
 
 const soundMap: Record<string, AVPlaybackSource> = {
-    "chattur/audio-0.mp3": require("@/assets/voicelines/chattur/audio-0.mp3"),
-    "chattur/audio-1.mp3": require("@/assets/voicelines/chattur/audio-1.mp3"),
-    "chattur/audio-2.mp3": require("@/assets/voicelines/chattur/audio-2.mp3"),
-    "chattur/audio-3.mp3": require("@/assets/voicelines/chattur/audio-3.mp3"),
-    "chattur/audio-4.mp3": require("@/assets/voicelines/chattur/audio-4.mp3"),
-    "chattur/audio-5.mp3": require("@/assets/voicelines/chattur/audio-5.mp3"),
-    "chattur/audio-6.mp3": require("@/assets/voicelines/chattur/audio-6.mp3"),
-    "chattur/audio-7.mp3": require("@/assets/voicelines/chattur/audio-7.mp3"),
-    "chattur/audio-8.mp3": require("@/assets/voicelines/chattur/audio-8.mp3"),
-    "chattur/audio-9.mp3": require("@/assets/voicelines/chattur/audio-9.mp3"),
-    "chattur/audio-10.mp3": require("@/assets/voicelines/chattur/audio-10.mp3"),
-    "chattur/audio-11.mp3": require("@/assets/voicelines/chattur/audio-11.mp3"),
-    "chattur/audio-12.mp3": require("@/assets/voicelines/chattur/audio-12.mp3"),
-    "chattur/audio-13.mp3": require("@/assets/voicelines/chattur/audio-13.mp3")
+    "chattur/audio_0.mp3": require("@/assets/voicelines/chattur/audio_0.mp3"),
+    "chattur/audio_1.mp3": require("@/assets/voicelines/chattur/audio_1.mp3"),
+    "chattur/audio_2.mp3": require("@/assets/voicelines/chattur/audio_2.mp3"),
+    "chattur/audio_3.mp3": require("@/assets/voicelines/chattur/audio_3.mp3"),
+    "chattur/audio_4.mp3": require("@/assets/voicelines/chattur/audio_4.mp3"),
+    "chattur/audio_5.mp3": require("@/assets/voicelines/chattur/audio_5.mp3"),
+    "chattur/audio_6.mp3": require("@/assets/voicelines/chattur/audio_6.mp3"),
+    "chattur/audio_7.mp3": require("@/assets/voicelines/chattur/audio_7.mp3"),
+    "chattur/audio_8.mp3": require("@/assets/voicelines/chattur/audio_8.mp3"),
+    "chattur/audio_9.mp3": require("@/assets/voicelines/chattur/audio_9.mp3"),
+    "chattur/audio_10.mp3": require("@/assets/voicelines/chattur/audio_10.mp3"),
+    "chattur/audio_11.mp3": require("@/assets/voicelines/chattur/audio_11.mp3"),
+    "chattur/audio_12.mp3": require("@/assets/voicelines/chattur/audio_12.mp3"),
+    "chattur/audio_13.mp3": require("@/assets/voicelines/chattur/audio_13.mp3")
 }
 
 export default soundMap;
